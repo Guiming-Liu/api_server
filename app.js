8c16218c7d7902466e066ed19165c47b7b137192
Guiming <781517026@qq.com>
2023-02-15 20:16:56 +0800

    登录注册

--- app.js
+++ app.js
@@ -8,13 +8,35 @@ const cors = require('cors')
 // 将cors注册为全局中间件
 app.use(cors())
 // 配置表单数据的中间件
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
+// 声明一个全局中间件，为res对象挂载res.cc()函数，用于调用res.send()，向客户端响应处理失败的结果
+app.use((req, res, next) => {
+    // status = 0 为成功，status = 1 为失败；默认将status的值设置为1，方便处理失败的情况
+    res.cc = (err, status = 1) => {
+        res.send({
+            // 状态
+            status,
+            // 状态描述，判断err是错误对象还是字符串
+            message: err instanceof Error ? err.message : err
+        })
+    }
+    next()
+})
+// 声明全局错误级别的中间件，捕获验证失败的错误，并把验证失败的结果响应给客户端
+const joi = require('joi')
+app.use((err, req, res, next) => {
+    // 数据验证失败
+    if (err instanceof joi.ValidationError) return res.cc(err)
+    // 未知错误
+    res.cc(err)
+})
+
 
 // 导入并使用用户路由模块
 const userRouter = require('./router/user')
 app.use('/api', userRouter)
 
 // 调用app.listen方法，指定端口号并启动web服务器
-app.listen(3007,function() {
+app.listen(3007, function () {
     console.log('api server running at http://127.0.0.1:3007')
 })
--- router_handler/user.js
+++ router_handler/user.js
@@ -17,7 +17,8 @@ exports.regUser = (req, res) => {
     const userInfo = req.body
     // 判断数据是否合法
     if (!userInfo.username || !userInfo.password) {
-        return res.send({ status: 1, message: '用户名和密码不能为空！' })
+        // return res.send({ status: 1, message: '用户名和密码不能为空！' })
+        return res.cc('用户名和密码不能为空！')
     }
 
     // 1.2检测用户名是否被占用
@@ -27,11 +28,13 @@ exports.regUser = (req, res) => {
     db.query(occupySql, userInfo.username, (err, results) => {
         // 执行sql语句失败
         if (err) {
-            return res.send({ status: 1, message: err.message })
+            // return res.send({ status: 1, message: err.message })
+            return res.cc(err)
         }
         // 用户名被占用
         if (results.length > 0) {
-            return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+            // return res.send({ status: 1, message: '用户名被占用，请更换其他用户名！' })
+            return res.cc('用户名被占用，请更换其他用户名！')
         }
         // 用户名可用，继续后面的流程...
 
@@ -45,13 +48,15 @@ exports.regUser = (req, res) => {
         // 调用db.query()执行SQL语句，插入新用户
         db.query(insertSql, { username: userInfo.username, password: userInfo.password }, (err, results) => {
             // 执行sql语句失败
-            if (err) return res.send({ status: 1, message: err.message })
+            // if (err) return res.send({ status: 1, message: err.message })
+            if (err) return res.cc(err)          
             // 执行sql语句成功，但影响行不为1
             if (results.affectedRows !== 1) {
-                return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+                // return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+                return res.cc('注册用户失败，请稍后再试！')
             }
             // 注册成功
-            res.send({ status: 1, message: '注册成功！' })
+            res.cc('注册成功！')
         })
     })
 
