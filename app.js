b5d603075a3a87aac024f15b1b5edd7d1b368a57
Guiming <781517026@qq.com>
2023-02-15 16:25:20 +0800

    初始化

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 // 导入express模块
-import express from 'express'
+const express = require('express')
 // 创建exress的服务器实例
 const app = express()
 
-// write your code here...
+// 导入 cors 中间件
+const cors = require('cors')
+// 将cors注册为全局中间件
+app.use(cors())
+// 配置表单数据的中间件
+app.use(express.urlencoded({extended:false}))
 
+// 导入并使用用户路由模块
+const userRouter = require('./router/user')
+app.use('/api', userRouter)
 
 // 调用app.listen方法，指定端口号并启动web服务器
 app.listen(3007,function() {
